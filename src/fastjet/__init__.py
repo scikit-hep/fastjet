d9b8090d65c70d44a9303d3a4817922dcc7f27e9
Jim Pivarski <jpivarski@gmail.com>
2021-04-15 10:36:42 -0500

    Flake8.

--- src/fastjet/__init__.py
+++ src/fastjet/__init__.py
@@ -1,6 +1,6 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/fastjet/blob/main/LICENSE
 
-import fastjet._ext
+import fastjet._ext  # noqa: F401
 from fastjet.version import __version__
 
 __all__ = ("__version__",)
