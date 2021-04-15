61fb6df887d8a4a68f96404854bc1e90c4758093
Jim Pivarski <jpivarski@gmail.com>
2021-04-15 17:19:59 -0500

    Now compiles fastjet-core and can instantiate SWIG objects in Python.

--- tests/test_0001-merge-in-aryan26roy-fastjet.py
+++ tests/test_0001-merge-in-aryan26roy-fastjet.py
@@ -1,13 +1,8 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/fastjet/blob/main/LICENSE
 
 import fastjet
-import fastjet._ext
+import fastjet._swig
 
 
-def test_pybind11():
-    assert fastjet._ext.add(1, 2) == 3
-    assert fastjet._ext.subtract(1, 2) == -1
-
-
-def test_version():
-    assert fastjet.__version__
+def test_swig_bindings():
+    fastjet._swig.PseudoJet(1, 2, 3, 4)
