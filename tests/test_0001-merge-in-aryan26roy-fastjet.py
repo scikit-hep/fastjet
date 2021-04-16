bd9ebdddf8deac393b3126a117e02503696c4ad0
Jim Pivarski <jpivarski@gmail.com>
2021-04-15 20:41:38 -0500

    Fixed isort.

--- tests/test_0001-merge-in-aryan26roy-fastjet.py
+++ tests/test_0001-merge-in-aryan26roy-fastjet.py
@@ -1,8 +1,8 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/fastjet/blob/main/LICENSE
 
 import fastjet
-import fastjet._swig
 import fastjet._ext
+import fastjet._swig
 
 
 def test_swig_bindings():
