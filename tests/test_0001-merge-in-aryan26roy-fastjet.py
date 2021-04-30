71b7cc24a98f96a91597a18afd5c0f76b8486a99
aryan26roy <aryanroy5678@gmail.com>
2021-04-30 15:15:59 +0530

    modified test

--- tests/test_0001-merge-in-aryan26roy-fastjet.py
+++ tests/test_0001-merge-in-aryan26roy-fastjet.py
@@ -1,5 +1,5 @@
 # BSD 3-Clause License; see https://github.com/scikit-hep/fastjet/blob/main/LICENSE
-import awkward as ak
+import numpy as np
 
 import fastjet
 import fastjet._ext
@@ -11,12 +11,8 @@ def test_swig_bindings():
 
 
 def test_pybind11():
-
-    array = ak.Array(
-        [
-            {"px": 1.12, "py": 2.11, "pz": 3.71, "E": 5.1},
-            {"px": 1.15, "py": 2.41, "pz": 3.18, "E": 52.1},
-            {"px": 11.1, "py": 24.1, "pz": 35.11, "E": 51.11},
-        ]
-    )
-    fastjet.pyjet.AwkwardClusterSequence(array, 0.8, "antikt_algorithm")
+    px = np.ndarray([1.12, 1.15, 11.1])
+    py = np.ndarray([2.11, 2.41, 24.1])
+    pz = np.ndarray([3.71, 3.18, 35.11])
+    E = np.ndarray([5.1, 52.1, 51.11])
+    fastjet._ext.interface(px, py, pz, E, 0.8, "antikt_algorithm")
