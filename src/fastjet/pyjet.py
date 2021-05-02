9b2adac9f490ba4cde4e5cb88c1301145621e27a
aryan26roy <50577809+aryan26roy@users.noreply.github.com>
2021-05-02 18:46:10 +0530

    changed outputs (#10)

--- src/fastjet/pyjet.py
+++ src/fastjet/pyjet.py
@@ -15,57 +15,36 @@ class AwkwardClusterSequence:
         pz = data["part0-node3-data"]
         E = data["part0-node4-data"]
         self.form = """ {
-    "class": "ListOffsetArray64",
-    "offsets": "i64",
-    "content": {
-        "class": "RecordArray",
-        "contents": {
-            "phi": {
-                "class": "NumpyArray",
-                "itemsize": 8,
-                "format": "d",
-                "primitive": "float64",
-                "form_key": "node2"
-            },
-            "pt": {
-                "class": "NumpyArray",
-                "itemsize": 8,
-                "format": "d",
-                "primitive": "float64",
-                "form_key": "node3"
-            },
-            "rap": {
-                "class": "NumpyArray",
-                "itemsize": 8,
-                "format": "d",
-                "primitive": "float64",
-                "form_key": "node4"
-            },
-            "constituents": {
-                "class": "ListOffsetArray64",
-                "offsets": "i64",
-                "content": {
-                    "class": "NumpyArray",
-                    "itemsize": 8,
-                    "format": "l",
-                    "primitive": "int64",
-                    "form_key": "node6"
-                },
-                "form_key": "node5"
-            }
+    "class": "RecordArray",
+    "contents": {
+        "phi": {
+            "class": "NumpyArray",
+            "itemsize": 8,
+            "format": "d",
+            "primitive": "float64",
+            "form_key": "node1"
         },
-        "parameters": {
-            "__record__": "Momentum4D"
+        "rap": {
+            "class": "NumpyArray",
+            "itemsize": 8,
+            "format": "d",
+            "primitive": "float64",
+            "form_key": "node2"
         },
-        "form_key": "node1"
+        "pt": {
+            "class": "NumpyArray",
+            "itemsize": 8,
+            "format": "d",
+            "primitive": "float64",
+            "form_key": "node3"
+        }
+    },
+    "parameters": {
+        "__record__": "Momentum4D"
     },
     "form_key": "node0"
 }
-
 """
-        self.out = fastjet._ext.interface(px, py, pz, E, R, algor)
-        # length = self.out["nevents"]
-        # self.out.pop("nevents")
-        # for key in self.out:
-        # self.out[key] = np.asarray(self.out[key], dtype = "float64")
-        # self.final = ak.from_buffers(self.form,length,self.out)
+        results = fastjet._ext.interface(px, py, pz, E, R, algor)
+        size = len(results["part0-node1-data"])
+        self.inclusive_jets = ak.from_buffers(self.form, size, results)
