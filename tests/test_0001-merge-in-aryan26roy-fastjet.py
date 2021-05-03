f5e0b94118f97230c6c2a749822fd3b40f2f0456
aryan26roy <aryanroy5678@gmail.com>
2021-05-03 16:29:48 +0530

    updated test

--- tests/test_0001-merge-in-aryan26roy-fastjet.py
+++ tests/test_0001-merge-in-aryan26roy-fastjet.py
@@ -15,4 +15,6 @@ def test_pybind11():
     py = np.array([2.11, 2.41, 24.1], dtype="float32")
     pz = np.array([3.71, 3.18, 35.11], dtype="float32")
     E = np.array([5.1, 52.1, 51.11], dtype="float32")
-    fastjet._ext.interface(px, py, pz, E, 0.8, "antikt_algorithm")
+    inps = {"algor": "anti-kt"}
+    inpf = {"R": 0.6}
+    fastjet._ext.interface(px, py, pz, E, inps, inpf)
