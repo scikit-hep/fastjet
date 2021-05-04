c8a472ba79309e2f9d3c6deb1385c91646ae5b5c
aryan26roy <50577809+aryan26roy@users.noreply.github.com>
2021-05-04 12:22:10 +0530

    Aryan/swig jetdef (#13)
    * using swig jet-def
    
    * updated test
    
    * better handling
    
    * linting

--- src/fastjet/pyjet.py
+++ src/fastjet/pyjet.py
@@ -8,17 +8,36 @@ __all__ = ("__version__",)
 
 class AwkwardClusterSequence:
     def __init__(self, data, jetdef):
-        self.params = jetdef.description().split()
-        algor = self.params[2]
-        Rv = float(self.params[7])
-        inps = {"algor": algor}
-        inpf = {"R": Rv}
+        self.jetdef = jetdef
+        inps, inpf = self.swig_to_params(self.jetdef)
         container, length, data = ak.to_buffers(data)
         # offsets = data["part0-node0-offsets"]
         px = data["part0-node1-data"]
         py = data["part0-node2-data"]
         pz = data["part0-node3-data"]
         E = data["part0-node4-data"]
+        results = fastjet._ext.interface(px, py, pz, E, inps, inpf)
+        self.inclusive_jets = self.construct_awkward_array(results)
+
+    def swig_to_params(self, jetdef):
+        params = jetdef.description().split()
+        if params[0] == "e+e-":
+            algor = params[0]
+            Rv = float(params[7])  # e+e- generalised
+        if params[0] == "e+e-" and params[2] == "(Durham)":
+            algor = "Durham"  # e+e- durham
+            Rv = 0
+        if params[2] == "generalised":
+            Rv = params[8]
+            algor = "genkt"  # genralised kt
+        else:
+            algor = params[2]
+            Rv = float(params[7])
+        inps = {"algor": algor}  # kt or antikt
+        inpf = {"R": Rv}
+        return inps, inpf
+
+    def construct_awkward_array(self, results):
         self.form = """ {
     "class": "RecordArray",
     "contents": {
@@ -50,6 +69,6 @@ class AwkwardClusterSequence:
     "form_key": "node0"
 }
 """
-        results = fastjet._ext.interface(px, py, pz, E, inps, inpf)
         size = len(results["part0-node1-data"])
-        self.inclusive_jets = ak.from_buffers(self.form, size, results)
+        out = ak.from_buffers(self.form, size, results)
+        return out
