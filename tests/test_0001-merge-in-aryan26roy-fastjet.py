ac8b1a41783f0fda1efe5b0b9eadb8cc71138575
aryan26roy <50577809+aryan26roy@users.noreply.github.com>
2021-05-11 12:37:57 +0530

    Update test_0001-merge-in-aryan26roy-fastjet.py (#21)

--- tests/test_0001-merge-in-aryan26roy-fastjet.py
+++ tests/test_0001-merge-in-aryan26roy-fastjet.py
@@ -16,6 +16,4 @@ def test_pybind11():
     pz = np.array([3.71, 3.18, 35.11], dtype="float32")
     E = np.array([5.1, 52.1, 51.11], dtype="float32")
     jetdef = fastjet._swig.JetDefinition(fastjet._swig.antikt_algorithm, 0.6)
-    inps = {"algor": "anti-kt"}
-    inpf = {"R": 0.6}
-    fastjet._ext.interface(px, py, pz, E, inps, inpf, jetdef)
+    fastjet._ext.interface(px, py, pz, E, jetdef)
