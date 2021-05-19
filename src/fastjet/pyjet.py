2a2c151d1d948692414a9c4c496f6bf5093d563f
Aryan Roy <aryanroy5678@gmail.com>
2021-05-19 18:02:35 +0530

    decoupled single and multievent and added multievent inclusive jets

--- src/fastjet/pyjet.py
+++ src/fastjet/pyjet.py
@@ -12,29 +12,31 @@ class AwkwardClusterSequence:
         self.jetdef = jetdef
         self.jagedness = self.check_jaggedness(data)
         # inps, inpf = self.swig_to_params(self.jetdef)
-        container, length, data = ak.to_buffers(data)
         # offsets = data["part0-node0-offsets"]
-        data = self.correct_byteorder(data)
         if self.jagedness == 0:
+            container, length, data = ak.to_buffers(data)
             px = data["part0-node1-data"]
             py = data["part0-node2-data"]
             pz = data["part0-node3-data"]
             E = data["part0-node4-data"]
-            self._results = fastjet._ext.interface(px, py, pz, E, jetdef)
-        if self.jagedness == 1:
-            px = data["part0-node2-data"]
-            py = data["part0-node3-data"]
-            pz = data["part0-node4-data"]
-            E = data["part0-node5-data"]
-            offsets = data["part0-node0-offsets"]
-            self._results = fastjet._ext.interfacemulti(px, py, pz, E, offsets, jetdef)
+            self._results = fastjet._ext.interface(
+                self.px, self.py, self.pz, self.E, jetdef
+            )
+        px, py, pz, E, offsets = self.extract_cons(data)
+        self.px = self.correct_byteorder(px)
+        self.py = self.correct_byteorder(py)
+        self.pz = self.correct_byteorder(pz)
+        self.E = self.correct_byteorder(E)
+        self.offsets = self.correct_byteorder(offsets)
+        self._results = fastjet._ext.interfacemulti(
+            self.px, self.py, self.pz, self.E, self.offsets, self.jetdef
+        )
 
     def correct_byteorder(self, data):
-        for keys in data:
-            if data[keys].dtype.byteorder == "=":
-                pass
-            else:
-                data[keys] = data[keys].dtype.newbyteorder("=")
+        if data.dtype.byteorder == "=":
+            pass
+        else:
+            data = data.dtype.newbyteorder("=")
         return data
 
     def check_jaggedness(self, data):
@@ -42,6 +44,15 @@ class AwkwardClusterSequence:
             return 1 + self.check_jaggedness(ak.Array(data.layout.content))
         else:
             return 0
+
+    def extract_cons(self, array):
+        px = np.asarray(ak.Array(array.layout.content, behavior=array.behavior).px)
+        py = np.asarray(ak.Array(array.layout.content, behavior=array.behavior).py)
+        pz = np.asarray(ak.Array(array.layout.content, behavior=array.behavior).pz)
+        E = np.asarray(ak.Array(array.layout.content, behavior=array.behavior).E)
+        off = np.asarray(array.layout.stops)
+        off = np.insert(off, 0, 0)
+        return px, py, pz, E, off
 
     @property
     def inclusive_jets(self):
@@ -61,7 +72,23 @@ class AwkwardClusterSequence:
 
             return out
         if self.jagedness == 1:
-            np_results = self._results.cse
+            np_results = self._results.to_numpy()
+            of = np.insert(np_results[-1], len(np_results[-1]), len(np_results[0]))
+            out = ak.Array(
+                ak.layout.ListOffsetArray64(
+                    ak.layout.Index64(of),
+                    ak.layout.RecordArray(
+                        (
+                            ak.layout.NumpyArray(np_results[0]),
+                            ak.layout.NumpyArray(np_results[1]),
+                            ak.layout.NumpyArray(np_results[2]),
+                            ak.layout.NumpyArray(np_results[3]),
+                        ),
+                        ("px", "py", "pz", "E"),
+                    ),
+                )
+            )
+            return out
 
     @property
     def unclustered_parts(self):
