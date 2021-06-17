ce05d32ad7e63d1571dc54f1d410f7a15fc46681
Aryan Roy <aryanroy5678@gmail.com>
2021-06-17 11:26:51 +0530

    added multiple standalone functions

--- src/fastjet/_utils.py
+++ src/fastjet/_utils.py
@@ -2,6 +2,8 @@ import awkward as ak
 
 import fastjet._swig
 
+# light wrapping for the functions to raise an error if the user inputs awkward arrays into functions meant for swig
+
 
 def sorted_by_E(data):
     if isinstance(data, ak.Array):
@@ -61,10 +63,7 @@ def sorted_by_rapidity(data):
 
 def join(*argv):
     if isinstance(argv[0], ak.Array):
-        for arg in argv:
-            if not isinstance(arg, ak.Array):
-                raise AttributeError("All arguments need to be of the same type")
-        # CODE FOR THIS NEEDS TO BE WRITTEN
+        raise TypeError("Use inbuilt methods for awkward arrays")
     else:
         for arg in argv:
             if isinstance(arg, ak.Array):
@@ -79,3 +78,45 @@ def join(*argv):
             return fastjet._swig.join(argv[0], argv[1], argv[2], argv[3])
         if len(argv) > 4:
             raise ValueError("Length exceeded")
+
+
+def dot_product(a, b):
+    if isinstance(a, ak.Array) or isinstance(b, ak.Array):
+        raise TypeError("Use inbuilt methods for Awkward Array")
+    else:
+        return fastjet._swig.dot_product(a, b)
+
+
+def sort_indices(indices, values):
+    if isinstance(indices, ak.Array) or isinstance(values, ak.Array):
+        raise TypeError("Use inbuilt methods for Awkward Array")
+    else:
+        return sort_indices(indices, values)
+
+
+def theta(a, b):
+    if isinstance(a, ak.Array) or isinstance(b, ak.Array):
+        raise TypeError("Use inbuilt methods for Awkward Array")
+    else:
+        return fastjet._swig.theta(a, b)
+
+
+def have_same_momentum(a, b):
+    if isinstance(a, ak.Array) or isinstance(b, ak.Array):
+        raise TypeError("Use inbuilt methods for Awkward Array")
+    else:
+        return fastjet._swig.have_same_momentum(a, b)
+
+
+def cos_theta(a, b):
+    if isinstance(a, ak.Array) or isinstance(b, ak.Array):
+        raise TypeError("Use inbuilt methods for Awkward Array")
+    else:
+        return fastjet._swig.cos_theta(a, b)
+
+
+def PtYPhiM(pt, y, phi, m):
+    if isinstance(a, ak.Array) or isinstance(b, ak.Array):
+        raise TypeError("Use inbuilt methods for Awkward Array")
+    else:
+        return fastjet._swig.PtYPhiM(pt, y, phi, m)
