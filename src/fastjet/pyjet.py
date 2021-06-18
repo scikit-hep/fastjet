16abe09f8e51d73397a05e6f0e84f320aeef77db
Aryan Roy <aryanroy5678@gmail.com>
2021-06-18 13:26:35 +0530

    Added some exception handling

--- src/fastjet/pyjet.py
+++ src/fastjet/pyjet.py
@@ -11,6 +11,10 @@ __all__ = ("__version__",)
 class AwkwardClusterSequence:
     def __init__(self, data, jetdef):
         """The base class for all clustering"""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
+        if not isinstance(jetdef, fastjet._swig.JetDefinition):
+            raise TypeError("JetDefinition is not of valid type")
         self._jetdef = jetdef
         self._jagedness = self._check_jaggedness(data)
         if self._check_listoffset(data):
@@ -100,34 +104,50 @@ class AwkwardClusterSequence:
 
     def exclusive_subjets(self, data, dcut=-1, nsub=-1):
         """Returns an Awkward Array of all subjets of the current jet (in the sense of the exclusive algorithm) that would be obtained when running the algorithm with the given dcut."""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.exclusive_subjets(data, dcut, nsub)
 
     def exclusive_subjets_up_to(self, data, nsub=0):
         """Returns the list of subjets obtained by unclustering the supplied jet down to nsub subjets (or all constituents if there are fewer than nsub)."""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.exclusive_subjets_up_to(data, nsub)
 
     def exclusive_subdmerge(self, data, nsub=0):
         """Returns the dij that was present in the merging nsub+1 -> nsub subjets inside this jet."""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.exclusive_subdmerge(data, nsub)
 
     def exclusive_subdmerge_max(self, data, nsub=0):
         """Returns the maximum dij that occurred in the whole event at the stage that the nsub+1 -> nsub merge of subjets occurred inside this jet."""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.exclusive_subdmerge_max(data, nsub)
 
     def n_exclusive_subjets(self, data, dcut=0):
         """Returns the size of exclusive_subjets(...); still n ln n with same coefficient, but marginally more efficient than manually taking len(exclusive_subjets)"""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.n_exclusive_subjets(data, dcut)
 
     def has_parents(self, data):
         """if the jet has parents in the clustering, it returns true"""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.has_parents(data)
 
     def has_child(self, data):
         """if the jet has children in the clustering, it returns true"""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.has_child(data)
 
     def jet_scale_for_algorithm(self, data):
         """Returns the scale associated with a jet as required for this clustering algorithm (kt^2 for the kt-algorithm, 1 for the Cambridge algorithm)."""
+        if not isinstance(data, ak.Array):
+            raise TypeError("The input data is not an Awkward Array")
         return self._internalrep.jet_scale_for_algorithm(data)
 
     def unique_history_order(self):
