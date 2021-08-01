8f134263366b1e9489a91b23d944f9871c6405c1
Aryan Roy <aryanroy5678@gmail.com>
2021-08-01 14:22:52 +0530

    added support for multiple listoffset arrays of events

--- src/fastjet/_generalevent.py
+++ src/fastjet/_generalevent.py
@@ -8,18 +8,24 @@ class _classgeneralevent:
     def __init__(self, data, jetdef):
         self.jetdef = jetdef
         self.data = data
+        self._mod_data = data
         self._bread_list = []
+        self._clusterable_level = []
+        self._results = []
         self.multi_layered_listoffset(self.data, ())
-        self._clusterable_level = ak.Array(
-            self._clusterable_level.layout.toListOffsetArray64(True)
-        )
-        px, py, pz, E, offsets = self.extract_cons(self._clusterable_level)
-        px = self.correct_byteorder(px)
-        py = self.correct_byteorder(py)
-        pz = self.correct_byteorder(pz)
-        E = self.correct_byteorder(E)
-        offsets = self.correct_byteorder(offsets)
-        self._results = fastjet._ext.interfacemulti(px, py, pz, E, offsets, jetdef)
+        for i in range(len(self._clusterable_level)):
+            self._clusterable_level[i] = ak.Array(
+                self._clusterable_level[i].layout.toListOffsetArray64(True)
+            )
+            px, py, pz, E, offsets = self.extract_cons(self._clusterable_level[i])
+            px = self.correct_byteorder(px)
+            py = self.correct_byteorder(py)
+            pz = self.correct_byteorder(pz)
+            E = self.correct_byteorder(E)
+            offsets = self.correct_byteorder(offsets)
+            self._results.append(
+                fastjet._ext.interfacemulti(px, py, pz, E, offsets, jetdef)
+            )
 
     def _check_listoffset_subtree(self, data):
         out = isinstance(
@@ -102,7 +108,7 @@ class _classgeneralevent:
                 ):
                     crumb_list = crumb_list + (None,)
                     self._bread_list.append(crumb_list)
-                    self._clusterable_level = ak.Array(data.layout.content)
+                    self._clusterable_level.append(ak.Array(data.layout.content))
             elif self._check_indexed(
                 ak.Array(ak.Array(data.layout.content).layout.content),
             ):
@@ -122,7 +128,7 @@ class _classgeneralevent:
                     ):
                         crumb_list = crumb_list + (None,)
                         self._bread_list.append(crumb_list)
-                        self._clusterable_level = ak.Array(data.layout.content)
+                        self._clusterable_level.append(ak.Array(data.layout.content))
                 else:
                     crumb_list = crumb_list + (None,)
                     self.multi_layered_listoffset(
@@ -151,7 +157,12 @@ class _classgeneralevent:
         off = np.insert(off, 0, 0)
         return px, py, pz, E, off
 
-    def replace(self, layout):
+    def _replace_multi(self, layout):
+        for i in range(len(self._clusterable_level)):
+            self._mod_data = ak.Array(self.replace(layout, i, 0))
+        return self._mod_data
+
+    def replace(self, layout, cluster, level):
         if isinstance(
             layout,
             (
@@ -174,7 +185,7 @@ class _classgeneralevent:
                 and "pz" in attributes
                 and "E" in attributes
             ):
-                return self.out.layout
+                return self._out[cluster].layout
 
         elif (
             isinstance(
@@ -211,33 +222,33 @@ class _classgeneralevent:
                 and "pz" in attributes
                 and "E" in attributes
             ):
-                return self.out.layout
+                return self._out[cluster].layout
         elif self._check_listoffset(ak.Array(layout)):
             if isinstance(layout, ak.layout.ListOffsetArray64):
                 return ak.layout.ListOffsetArray64(
                     layout.offsets,
-                    self.replace(layout.content),
+                    self.replace(layout.content, cluster, level + 1),
                     layout.identities,
                     layout.parameters,
                 )
             if isinstance(layout, ak.layout.ListOffsetArray32):
                 return ak.layout.ListOffsetArray32(
                     layout.offsets,
-                    self.replace(layout.content),
+                    self.replace(layout.content, cluster, level + 1),
                     layout.identities,
                     layout.parameters,
                 )
             if isinstance(layout, ak.layout.ListOffsetArrayU32):
                 return ak.layout.ListOffsetArrayU32(
                     layout.offsets,
-                    self.replace(layout.content),
+                    self.replace(layout.content, cluster, level + 1),
                     layout.identities,
                     layout.parameters,
                 )
         elif isinstance(layout, ak.layout.ByteMaskedArray):
             return ak.layout.ByteMaskedArray(
                 layout.mask,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.valid_when,
                 layout.identities,
                 layout.parameters,
@@ -245,7 +256,7 @@ class _classgeneralevent:
         elif isinstance(layout, ak.layout.BitMaskedArray):
             return ak.layout.BitMaskedArray(
                 layout.mask,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.valid_when,
                 len(layout),
                 layout.lsb_order,
@@ -254,13 +265,13 @@ class _classgeneralevent:
             )
         elif isinstance(layout, ak.layout.UnmaskedArray):
             return ak.layout.UnmaskedArray(
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.RegularArray):
             return ak.layout.RegularArray(
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.size,
                 len(layout),
                 layout.identities,
@@ -270,7 +281,7 @@ class _classgeneralevent:
             return ak.layout.ListArray64(
                 layout.starts,
                 layout.stops,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
@@ -278,7 +289,7 @@ class _classgeneralevent:
             return ak.layout.ListArray32(
                 layout.starts,
                 layout.stops,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
@@ -286,48 +297,48 @@ class _classgeneralevent:
             return ak.layout.ListArrayU32(
                 layout.starts,
                 layout.stops,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.IndexedArray64):
             return ak.layout.IndexedArray64(
                 layout.index,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.IndexedArray32):
             return ak.layout.IndexedArray32(
                 layout.index,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.IndexedArrayU32):
             return ak.layout.IndexedArrayU32(
                 layout.index,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.IndexedOptionArray64):
             return ak.layout.IndexedOptionArray64(
                 layout.index,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.IndexedOptionArray32):
             return ak.layout.IndexedOptionArray32(
                 layout.index,
-                self.replace(layout.content),
+                self.replace(layout.content, cluster, level + 1),
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.RecordArray):
             return ak.layout.RecordArray(
-                [self.replace(x) for x in layout.contents],
+                [self.replace(x, cluster, level + 1) for x in layout.contents],
                 layout.recordlookup,
                 len(layout),
                 layout.identities,
@@ -335,14 +346,14 @@ class _classgeneralevent:
             )
         elif isinstance(layout, ak.layout.Record):
             return ak.layout.Record(
-                self.replace(layout.array),
+                self.replace(layout.array, cluster, level + 1),
                 layout.at,
             )
         elif isinstance(layout, ak.layout.UnionArray8_32):
             return ak.layout.UnionArray8_32(
                 layout.tags,
                 layout.index,
-                [self.replace(x) for x in layout.contents],
+                [self.replace(x, cluster, level + 1) for x in layout.contents],
                 layout.identities,
                 layout.parameters,
             )
@@ -350,7 +361,7 @@ class _classgeneralevent:
             return ak.layout.UnionArray8_U32(
                 layout.tags,
                 layout.index,
-                [self.replace(x) for x in layout.contents],
+                [self.replace(x, cluster, level + 1) for x in layout.contents],
                 layout.identities,
                 layout.parameters,
             )
@@ -358,50 +369,59 @@ class _classgeneralevent:
             return ak.layout.UnionArray8_64(
                 layout.tags,
                 layout.index,
-                [self.replace(x) for x in layout.contents],
+                [self.replace(x, cluster, level + 1) for x in layout.contents],
                 layout.identities,
                 layout.parameters,
             )
         elif isinstance(layout, ak.layout.VirtualArray):
-            return self.replace(layout.array)
+            return self.replace(layout.array, cluster, level + 1)
 
         if isinstance(layout, ak.partition.PartitionedArray):
             return ak.partition.IrregularlyPartitionedArray(
-                [self.replace(x) for x in layout.partitions]
+                [self.replace(x, cluster, level + 1) for x in layout.partitions]
             )
         else:
             raise AssertionError(layout)
 
     def inclusive_jets(self, min_pt):
-        np_results = self._results.to_numpy(min_pt)
-        of = np.insert(np_results[-1], len(np_results[-1]), len(np_results[0]))
-        self.out = ak.Array(
-            ak.layout.ListOffsetArray64(
-                ak.layout.Index64(of),
-                ak.layout.RecordArray(
-                    (
-                        ak.layout.NumpyArray(np_results[0]),
-                        ak.layout.NumpyArray(np_results[1]),
-                        ak.layout.NumpyArray(np_results[2]),
-                        ak.layout.NumpyArray(np_results[3]),
-                    ),
-                    ("px", "py", "pz", "E"),
-                ),
+        self._out = []
+        for i in range(len(self._clusterable_level)):
+            np_results = self._results[i].to_numpy(min_pt)
+            of = np.insert(np_results[-1], len(np_results[-1]), len(np_results[0]))
+            self._out.append(
+                ak.Array(
+                    ak.layout.ListOffsetArray64(
+                        ak.layout.Index64(of),
+                        ak.layout.RecordArray(
+                            (
+                                ak.layout.NumpyArray(np_results[0]),
+                                ak.layout.NumpyArray(np_results[1]),
+                                ak.layout.NumpyArray(np_results[2]),
+                                ak.layout.NumpyArray(np_results[3]),
+                            ),
+                            ("px", "py", "pz", "E"),
+                        ),
+                    )
+                )
             )
-        )
-        res = ak.Array(self.replace(self.data.layout))
+        res = ak.Array(self._replace_multi(self._mod_data.layout))
         return res
 
     def constituent_index(self, min_pt):
-        np_results = self._results.to_numpy_with_constituents(min_pt)
-        off = np.insert(np_results[-1], 0, 0)
-        out = ak.Array(
-            ak.layout.ListOffsetArray64(
-                ak.layout.Index64(np_results[0]), ak.layout.NumpyArray(np_results[1])
+        self._out = []
+        for i in range(len(self._clusterable_level)):
+            np_results = self._results[i].to_numpy_with_constituents(min_pt)
+            off = np.insert(np_results[-1], 0, 0)
+            out = ak.Array(
+                ak.layout.ListOffsetArray64(
+                    ak.layout.Index64(np_results[0]),
+                    ak.layout.NumpyArray(np_results[1]),
+                )
             )
-        )
-        self.out = ak.Array(
-            ak.layout.ListOffsetArray64(ak.layout.Index64(off), out.layout)
-        )
-        res = ak.Array(self.replace(self.data.layout))
+            self._out.append(
+                ak.Array(
+                    ak.layout.ListOffsetArray64(ak.layout.Index64(off), out.layout)
+                )
+            )
+        res = ak.Array(self._replace_multi(self.data.layout))
         return res
