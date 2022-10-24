8b37967ba030ff8805e38fdccc098255af49b88c
Christos Papageorgakis <Christos.Papageorgakis@cern.ch>
2022-10-24 17:35:02 +0200

    Two occurences have len+1 lengths - Allocate appropriate memory

--- src/_ext.cpp
+++ src/_ext.cpp
@@ -1682,7 +1682,7 @@ PYBIND11_MODULE(_ext, m) {
 
         auto off = py::array(py::buffer_info(nullptr, sizeof(int), py::format_descriptor<int>::value, 1, {len+1}, {sizeof(int)}));
         auto bufoff = off.request();
-        int *ptroff = (int *)realloc(bufoff.ptr, len*sizeof(int)+1);;
+        int *ptroff = (int *)realloc(bufoff.ptr, (len+1)*sizeof(int)+1);;
         size_t idxe = 0;
         *ptroff = 0;
         ptroff++;
@@ -1791,7 +1791,7 @@ PYBIND11_MODULE(_ext, m) {
 
         auto off = py::array(py::buffer_info(nullptr, sizeof(int), py::format_descriptor<int>::value, 1, {len+1}, {sizeof(int)}));
         auto bufoff = off.request();
-        int *ptroff = (int *)realloc(bufoff.ptr, len*sizeof(int)+1);;
+        int *ptroff = (int *)realloc(bufoff.ptr, (len+1)*sizeof(int)+1);;
         size_t idxe = 0;
         *ptroff = 0;
         ptroff++;
