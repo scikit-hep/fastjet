352bd611ad5fcfbd699789350cf4ff0653bb1e60
Chris Papageorgakis <chrispap95@gmail.com>
2022-10-25 11:52:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ def get_version() -> str:
 
 
 class FastJetBuild(setuptools.command.build_ext.build_ext):
-    def build_extensions(self):
+    def build_extensions(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         if not OUTPUT.exists():
             zip_filename = DIR / pathlib.Path(CGAL_ZIP).parts[-1]
 
