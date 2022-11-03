b9f1f5b2e2c1c0617ed2ad94b82593fca09b3225
Chris Papageorgakis <chrispap95@gmail.com>
2022-11-03 18:32:07 +0100

    Rename patch files

--- setup.py
+++ setup.py
@@ -56,14 +56,14 @@ class FastJetBuild(setuptools.command.build_ext.build_ext):
             # Patch for FastJet core version 3.4.0
             # To be removed when https://gitlab.com/fastjet/fastjet/-/merge_requests/1 is merged upstream
             subprocess.run(
-                ["patch", "pyinterface/fastjet.i", DIR / "patch1.txt"],
+                ["patch", "pyinterface/fastjet.i", DIR / "patch_fastjet_i.txt"],
                 cwd=FASTJET,
             )
 
             # Patch for segfault of LimitedWarning
             # For more info see https://github.com/scikit-hep/fastjet/pull/131
             subprocess.run(
-                ["patch", "src/ClusterSequence.cc", DIR / "patch2.txt"],
+                ["patch", "src/ClusterSequence.cc", DIR / "patch_clustersequence.txt"],
                 cwd=FASTJET,
             )
 
