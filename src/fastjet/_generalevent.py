f8ebc1251a2b7ea0a996f163fea2d7656f6c75e7
Javier Duarte <jduarte@ucsd.edu>
2023-03-07 10:47:46 -0800

    Update _generalevent.py

--- src/fastjet/_generalevent.py
+++ src/fastjet/_generalevent.py
@@ -311,7 +311,8 @@ class _classgeneralevent:
         ):
             warnings.formatwarning = fastjet.formatwarning
             warnings.warn(
-                "dcut and exclusive jets for jet-finders other than kt, C/A or genkt with p>=0 should be interpreted with care."
+                "dcut and exclusive jets for jet-finders other than kt, C/A or genkt with p>=0 should be interpreted with care.",
+                stacklevel=2,
             )
         return
 
