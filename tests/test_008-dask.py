7b8739e81aa2465dad5e4e3e7065ec01a3c0e61e
Lindsey Gray <lindsey.gray@gmail.com>
2023-06-13 06:00:26 -0500

    try using dask client in test

--- tests/test_008-dask.py
+++ tests/test_008-dask.py
@@ -5,57 +5,61 @@ import pytest  # noqa: F401
 import fastjet._pyjet  # noqa: F401
 
 dak = pytest.importorskip("dask_awkward")  # noqa: F401
+distributed = pytest.importorskip("distributed")
 vector = pytest.importorskip("vector")  # noqa: F401
 
 
 def test_multi():
-    array = ak.Array(
-        [
+    from distributed import Client
+
+    with Client() as client:
+        array = ak.Array(
             [
-                {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78},
-                {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
-                {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                [
+                    {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78},
+                    {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
+                    {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                ],
+                [
+                    {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78},
+                    {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
+                    {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                ],
+            ]
+        )
+        darray = dak.from_awkward(array, 1)
+        jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, 0.6)
+        cluster = fastjet._pyjet.DaskAwkwardClusterSequence(darray, jetdef)
+        inclusive_jets_out = [
+            [
+                {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5},
+                {"px": 64.65, "py": 127.41999999999999, "pz": 1086.48, "E": 48.68},
             ],
             [
-                {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78},
-                {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
-                {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5},
+                {"px": 64.65, "py": 127.41999999999999, "pz": 1086.48, "E": 48.68},
             ],
         ]
-    )
-    darray = dak.from_awkward(array, 1)
-    jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, 0.6)
-    cluster = fastjet._pyjet.DaskAwkwardClusterSequence(darray, jetdef)
-    inclusive_jets_out = [
-        [
-            {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5},
-            {"px": 64.65, "py": 127.41999999999999, "pz": 1086.48, "E": 48.68},
-        ],
-        [
-            {"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5},
-            {"px": 64.65, "py": 127.41999999999999, "pz": 1086.48, "E": 48.68},
-        ],
-    ]
-    assert inclusive_jets_out == cluster.inclusive_jets().compute().to_list()
-    constituents_output = [
-        [
-            [{"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78}],
+        assert inclusive_jets_out == cluster.inclusive_jets().compute().to_list()
+        constituents_output = [
             [
-                {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
-                {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                [{"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78}],
+                [
+                    {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
+                    {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                ],
             ],
-        ],
-        [
-            [{"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78}],
             [
-                {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
-                {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                [{"px": 1.2, "py": 3.2, "pz": 5.4, "E": 2.5, "ex": 0.78}],
+                [
+                    {"px": 32.2, "py": 64.21, "pz": 543.34, "E": 24.12, "ex": 0.35},
+                    {"px": 32.45, "py": 63.21, "pz": 543.14, "E": 24.56, "ex": 0.0},
+                ],
             ],
-        ],
-    ]
-    assert cluster.constituents().compute().to_list() == constituents_output
-    constituent_index_out = [[[0], [1, 2]], [[0], [1, 2]]]
-    assert constituent_index_out == cluster.constituent_index().compute().to_list()
+        ]
+        assert cluster.constituents().compute().to_list() == constituents_output
+        constituent_index_out = [[[0], [1, 2]], [[0], [1, 2]]]
+        assert constituent_index_out == cluster.constituent_index().compute().to_list()
 
 
 def test_single():
