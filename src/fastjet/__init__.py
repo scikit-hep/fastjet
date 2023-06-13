ff4c07399f984ca9e5e2c6db2eff456a6ac0f904
Lindsey Gray <lindsey.gray@gmail.com>
2023-06-13 09:56:58 -0500

    no item assignment for tuple

--- src/fastjet/__init__.py
+++ src/fastjet/__init__.py
@@ -221,7 +221,7 @@ class JetDefinition(JetDefinitionNoCast):
             new_kwargs = kwargs.copy()
             new_kwargs["R_in"] = R_in
         else:
-            new_args = args[:]
+            new_args = (args[0], R_in, *args[2:])
             new_args[1] = R_in
 
         self.args = new_args
