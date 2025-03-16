03377d7096fd2ed8d234d171a9bed028de0455a2
Lindsey Gray <lindsey.gray@gmail.com>
2025-03-16 13:04:37 -0500

    original recipe here.

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ class FastJetBuild(setuptools.command.build_ext.build_ext):
                 "--disable-auto-ptr",
                 "--enable-allcxxplugins",
                 "--enable-cgal",
+                "--enable-cgal-header-only",
                 f"--with-cgaldir={cgal_dir}",
                 "--enable-swig",
                 "--enable-pyext",
