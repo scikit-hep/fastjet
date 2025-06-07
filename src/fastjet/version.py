f69474323b0c39cf8e477fe1a475062c51e02348
Lindsey Gray <lindsey.gray@gmail.com>
2025-06-07 14:30:08 -0400

    scikit-build-core based wheels using fastjet cmake; linux, macos, windows

--- src/fastjet/version.py
+++
@@ -1,9 +0,0 @@
-# BSD 3-Clause License; see https://github.com/scikit-hep/fastjet/blob/main/LICENSE
-
-import re
-
-__version__ = "3.4.3.1"
-version = __version__
-version_info = tuple(re.split(r"[-\.]", __version__))
-
-del re
